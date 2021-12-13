5f47dc46c8d25faeb031208cf82f2daad2a4dfef
philipuuu <topgun0415@naver.com>
2021-12-13 18:34:15 +0900

    Javascript basic usage finish

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 /** @format */
+
+const h1 = document.querySelector('.hello:first-child h1');
+
+h1.addEventListener('click', () => {
+  const clickActive = 'active'; // .active css가 적용되도록 className에다가 active만 추가해주는 함수
+
+  if (h1.classList.contains(clickActive)) {
+    h1.classList.remove(clickActive);
+  } else {
+    h1.classList.add(clickActive);
+  }
+});
