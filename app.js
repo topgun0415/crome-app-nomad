005ef50deec8db6ca5956e7ffc363ccc919b079d
philipuuu <topgun0415@naver.com>
2021-12-18 19:21:31 +0900

    Refactor: Local Storage & Function refactor

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 /** @format */
+// DoucmentSelectors
+const $loginForm = document.querySelector('#login-form');
+const $loginInput = document.querySelector('#login-form input');
+const $greeting = document.querySelector('#greeting');
+// String value
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
+// LocalStorage
+const savedUserName = localStorage.getItem(USERNAME_KEY);
+// Function
+function paintGreetings(username) {
+  $greeting.innerText = `Hello! ${username}!`;
+  // 새로고침을 하여도 localStorage에 있는 데이터를 불러와 게속해서 유저정보를 표시해준다
+  $greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
-const $loginForm = document.querySelector('.login-form');
-const $loginInput = document.querySelector('.login-form input');
-
-const $link = document.querySelector('a');
-
-$loginForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  const userName = $loginInput.value;
-  console.log(userName);
-});
-
-$link.addEventListener('click', (e) => {
-  e.preventDefault();
-});
+if (savedUserName === null) {
+  $loginForm.classList.remove(HIDDEN_CLASSNAME); // localStroage에 없으면
+  $loginForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const userName = $loginInput.value;
+    localStorage.setItem(USERNAME_KEY, userName);
+    $loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(userName);
+    // $greeting.innerText = `Hello! ${userName}!`;
+    // $greeting.classList.remove(HIDDEN_CLASSNAME);
+  });
+} else {
+  paintGreetings(savedUserName);
+}
