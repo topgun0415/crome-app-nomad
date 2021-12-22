e8ccf7e7e5fa442fb2515fd1d82d6fd6f635f1bc
philipuuu <topgun0415@naver.com>
2021-12-23 02:30:01 +0900

    Feat : background's random img create

---
+++ js/background.js
@@ -0,0 +1,13 @@
+/** @format */
+
+// make a Array of img files
+const images = ['0.jpeg', '1.jpeg', '2.jpeg', '3.jpeg', '4.jpeg'];
+// use Radom method to put property into variable
+const randomImg = images[Math.floor(Math.random() * images.length)];
+
+// put HTML document(DOM) create element of 'img'
+const backgroundImg = document.createElement('img');
+// put address of img file
+backgroundImg.src = `img/${randomImg}`;
+// finally control Javascript to put tag into html by using appendChild (There is also prepend method)
+document.body.appendChild(backgroundImg);
