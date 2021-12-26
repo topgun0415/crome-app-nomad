71646894a5e098d7eb50081c1b92a8144bb92da6
philipuuu <topgun0415@naver.com>
2021-12-27 00:32:15 +0900

    Fix : ToDoList function fix

--- js/todolist.js
+++ js/todolist.js
@@ -1,30 +1,61 @@
 /** @format */
-
+// QuerySelector
 const $todoForm = document.querySelector('#to-do-form');
 const $todoInput = document.querySelector('#to-do-form input');
 const $todoList = document.querySelector('#to-do-list');
 
+// Variables
+let List = [];
+const LIST_KEY = 'List';
+
+// LocalStorage
+const savedList = localStorage.getItem(LIST_KEY);
+console.log(savedList);
+
+if (savedList !== null) {
+  const parsedList = JSON.parse(localStorage.getItem(LIST_KEY));
+  List = parsedList;
+  parsedList.forEach(paintToDo);
+}
+
+// localStorage saving function
+function saveList() {
+  localStorage.setItem(LIST_KEY, JSON.stringify(List));
+}
+
+// submit function
 $todoForm.addEventListener('submit', (e) => {
   e.preventDefault();
   // make sure that Javascipt's excution context is above to below
   const lists = $todoInput.value; // input value is added to lists variable
   $todoInput.value = ''; // after submit event processed, value will be empty
-  paintToDo(lists);
+  const listsObject = {
+    text: lists,
+    id: Date.now(),
+  };
+  List.push(listsObject);
+  paintToDo(listsObject);
+  saveList();
 });
 
 // callback function
-function paintToDo(lists) {
+function paintToDo(listsObject) {
   const li = document.createElement('li'); // make a li tag in html right away
+  li.id = listsObject.id; // 지우는 id 값
   const span = document.createElement('span');
   const button = document.createElement('button');
   li.appendChild(span);
   li.appendChild(button);
-  span.innerText = lists;
+  span.innerText = listsObject.text;
   button.innerText = '❌';
   button.addEventListener('click', (e) => {
     const delList = e.target.parentElement;
-    console.log(delList);
     delList.remove();
+    List = List.filter((list) => {
+      return list.id !== parseInt(li.id);
+    });
+    // List = List.filter((list) => list.id !== parseInt(li.id));
+    saveList();
   });
   $todoList.appendChild(li);
 }
